133ae856e7e7c13f64cd7322419b05d4e0b28104
aleksdesiigner <aleksdesiigner@gmail.com>
2020-07-23 19:02:29 +0300

    change scripts location

--- requests.js
+++ requests.js
@@ -18,3 +18,26 @@ document
          }
       });
    });
+
+// SEARCH INPUT
+
+document.querySelector('#filterInput').oninput = function () {
+   let searchValue = this.value.trim();
+   let searchItems = document.querySelectorAll('#requests .req-card')
+
+   if (searchValue != '') {
+      searchItems.forEach(function (elem) {
+         if (elem.innerHTML.search(searchValue) == -1) {
+            elem.classList.add('hide');
+
+         } else {
+            elem.classList.remove('hide');
+         }
+      })
+   } else {
+      searchItems.forEach(function (elem) {
+         elem.classList.remove('hide');
+      });
+   }
+
+}
--- script/script.js
+++ script/script.js
@@ -113,30 +113,6 @@ $("#btn-send").click(function (e) {
 
 //====================================================================
 
-// SEARCH INPUT
-
-document.querySelector('#filterInput').oninput = function () {
-  let searchValue = this.value.trim();
-  let searchItems = document.querySelectorAll('#requests .req-card')
-
-  if (searchValue != '') {
-    searchItems.forEach(function (elem) {
-      if (elem.innerHTML.search(searchValue) == -1) {
-        elem.classList.add('hide');
-
-      } else {
-        elem.classList.remove('hide');
-      }
-    })
-  } else {
-    searchItems.forEach(function (elem) {
-      elem.classList.remove('hide');
-    });
-  }
-
-}
-
-
 $('.req-card').on('click', function (event) {
   window.location.href = 'reqdetails.html';
 });
